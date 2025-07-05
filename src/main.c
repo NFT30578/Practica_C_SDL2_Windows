17f54816db6f6ac56489562d8cd330ff9f4e2f7b
NFT30578 <victordiaz.saldano@gmail.com>
2025-07-05 19:09:28 +0000

    creacion y configuracion de task.json

--- src/main.c
+++ src/main.c
@@ -1,63 +1,9 @@
 #include <stdio.h>
 #include <SDL.h>
-#include "../SDL2_ttf-2.0.13/SDL_ttf.h"
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-#define FONT_SIZE 32
-#define FONT_PATH "/mnt/SDCARD/Themes/SPRUCE/nunwen.ttf"
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER | SDL_INIT_EVENTS) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
-    if (TTF_Init() == -1) {
-        printf("TTF_Init Error: %s\n", TTF_GetError());
-        SDL_Quit();
-        return 1;
-    }
-    SDL_Window* window = SDL_CreateWindow("Hola Mundo SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_HEIGHT, WINDOW_WIDTH, SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
-    if (!window) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        TTF_Quit();
-        SDL_Quit();
-        return 1;
-    }
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        TTF_Quit();
-        SDL_Quit();
-        return 1;
-    }
-    SDL_RenderSetScale(renderer, 1.0, (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT);
-    TTF_Font* font = TTF_OpenFont(FONT_PATH, FONT_SIZE);
-    if (!font) {
-        printf("TTF_OpenFont Error: %s\n", TTF_GetError());
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        TTF_Quit();
-        SDL_Quit();
-        return 1;
-    }
-    SDL_Color color = {255, 255, 255};
-    SDL_Surface* surface = TTF_RenderText_Solid(font, "Hola Mundo", color);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-    SDL_Rect dstrect = {WINDOW_WIDTH/2 - 100, WINDOW_HEIGHT/2 - 32, 200, 64};
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(3000);
-    SDL_DestroyTexture(texture);
-    TTF_CloseFont(font);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
-    SDL_Quit();
-    //
+    // Inicializar SDL
+    SDL_init(SDL_INIT_VIDEO);
     return 0;
 }
