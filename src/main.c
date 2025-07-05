b93f5e2a9a4e8d28a3cea5e67d8150edca165f1d
NFT30578 <victordiaz.saldano@gmail.com>
2025-07-05 17:53:26 -0400

    eliminar archivos que no sirven para la compilacion en pc local

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
 int main(int argc, char* argv[]) {
     // Inicializar SDL
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* win = SDL_CreateWindow("Hola Victor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
     return 0;
 }
