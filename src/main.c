452d73dadd30bf5eab9d7f9ac3ed7d667aaf9eb4
NFT30578 <victordiaz.saldano@gmail.com>
2025-07-05 20:03:52 +0000

    listo, pero no abre por que codespace no tiene para abrir ventanas

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <SDL2/SDL2-2.26.1/x86_64-w64-mingw32/include/SDL2/SDL.h>
+#include <SDL.h>
 
 int main(int argc, char* argv[]) {
     // Inicializar SDL
-    SDL_init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
